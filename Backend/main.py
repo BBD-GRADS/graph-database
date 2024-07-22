3e273f9d46f7dc5070e0c813cd5b9728153328b4
JC Mouton <jacques.mouton@bbd.co.za>
2024-07-22 19:08:27 +0200

    neo4j>neptune

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 
+# Get Neo4j credentials from environment variables
+neo4j_uri = os.getenv("NEO4J_URI")
+neo4j_username = os.getenv("NEO4J_USERNAME")
+neo4j_password = os.getenv("NEO4J_PASSWORD")
+
+
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return f"Connected to Neo4j at {neo4j_uri}"
